987fc1d74b4e25b99649ba127d008b95ad40f48a
subir-ghosh-au19 <85132702+subir-ghosh-au26@users.noreply.github.com>
2022-07-07 00:20:47 +0530

    final

---
+++ frontend/src/component/layout/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Contact.css";
+import { Button } from "@material-ui/core";
+
+const Contact = () => {
+    return ( <
+        div className = "contactContainer" >
+        <
+        a className = "mailBtn"
+        href = "mailto:subirsgghosh@gmail.com" >
+        <
+        Button > Contact: subirsgghosh @gmail.com < /Button> < /
+        a > <
+        /div>
+    );
+};
+
+export default Contact;
